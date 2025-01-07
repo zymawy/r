600dee34d7be1e26f16db862c2a37fa0f0a3bcc7
zymawy <zymawy@gmail.com>
2025-01-07 09:28:41 +0300

    prccessing engine

--- api-payment/dist/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const paymentController_1 = require("../controllers/paymentController");
-const rbacMiddleware_1 = require("../middlewares/rbacMiddleware");
-const router = (0, express_1.Router)();
-router.post('/process', (0, rbacMiddleware_1.authorize)('user', 'write'), paymentController_1.handlePayment);
-exports.default = router;
